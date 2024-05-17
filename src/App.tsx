eabee057f7785b3a9f74f6b27f2160f03d057155
eundonghyeok <mephisto7191@naver.com>
2024-05-18 00:43:24 +0900

    Feat: page,components 기본틀 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,8 @@
 import "./App.css";
-import { Routes, Route, Navigate, Link } from "react-router-dom";
+import Router from "./components/Router";
 
 function App() {
-  return (
-    <>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/posts">Post List</Link>
-        </li>
-        <li>
-          <Link to="/posts/:id">Post Detail</Link>
-        </li>
-        <li>
-          <Link to="/posts/new">Post New</Link>
-        </li>
-        <li>
-          <Link to="/posts/edit:id">Post Edit</Link>
-        </li>
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
-      </ul>
-      <Routes>
-        <Route path="/" element={<h1>Home page</h1>} />
-        <Route path="/posts" element={<h1>Post List page</h1>} />
-        <Route path="/posts/:id" element={<h1>Post Detail page</h1>} />
-        <Route path="/posts/new" element={<h1>Post New page</h1>} />
-        <Route path="/posts/edit: id" element={<h1>Post Edit page</h1>} />
-        <Route path="/profile" element={<h1>Profile page</h1>} />
-        <Route path="/*" element={<Navigate replace to="/" />} />
-      </Routes>
-    </>
-  );
+  return <Router />;
 }
 
 export default App;
