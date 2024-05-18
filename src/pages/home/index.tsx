01dd45d148344d29823414520f1133ec0e0bad8d
eundonghyeok <mephisto7191@naver.com>
2024-05-19 00:06:15 +0900

    Feat: 홈화면 레이아웃

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,21 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
-  return <h1>Home page</h1>;
+  return (
+    <div>
+      <header>
+        <div>
+          <Link to="/post/new">글쓰기</Link>
+          <Link to="/post">게시글</Link>
+          <Link to="/profile">프로필</Link>
+        </div>
+      </header>
+      <div className="post-list">post list</div>
+      <footer>
+        <div>menu 1</div>
+        <div>menu 2</div>
+        <div>menu 3</div>
+      </footer>
+    </div>
+  );
 }
