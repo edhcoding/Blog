deff195b05eb3361cf50f689c515d02a7e904e47
eundonghyeok <mephisto7191@naver.com>
2024-05-20 00:57:55 +0900

    Feat: 홈화면 레이아웃 틀 완성

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,16 +5,46 @@ export default function Home() {
     <div>
       <header>
         <div>
-          <Link to="/post/new">글쓰기</Link>
-          <Link to="/post">게시글</Link>
+          <Link to="/posts/new">글쓰기</Link>
+          <Link to="/posts">게시글</Link>
           <Link to="/profile">프로필</Link>
         </div>
       </header>
-      <div className="post-list">post list</div>
+      <div className="post__navigation">
+        <div className="post__navigation--active">전체</div>
+        <div>나의 글</div>
+      </div>
+      <div className="post__list">
+        {[...Array(10)].map((e, index) => (
+          <div key={index} className="post__box">
+            <Link to={`/posts/${index}`}>
+              <div className="post__profile-box">
+                <div className="post__profile" />
+                <div className="post__author-name">은동혁</div>
+                <div className="post__date">2024.05.19 일요일</div>
+              </div>
+              <div className="post__title">게시글 {index}</div>
+              <div className="post__text">
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum."
+              </div>
+              <div className="post__utils-box">
+                <div className="post__edit">수정</div>
+                <div className="post__delete">삭제</div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
       <footer>
-        <div>menu 1</div>
-        <div>menu 2</div>
-        <div>menu 3</div>
+        <Link to="/posts/new">글쓰기</Link>
+        <Link to="/posts">게시글</Link>
+        <Link to="/profile">프로필</Link>
       </footer>
     </div>
   );
